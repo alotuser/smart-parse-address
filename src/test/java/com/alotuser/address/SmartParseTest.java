056cd692aa7a69eda1200904db851cafd88f4e31
alotuser <alotuser@163.com>
2026-06-22 09:15:29 +0800

    show Timer ms

--- src/test/java/com/alotuser/address/SmartParseTest.java
+++ src/test/java/com/alotuser/address/SmartParseTest.java
@@ -21,44 +21,39 @@ class SmartParseTest {
 		String textA1 = "收货人: 李丽 手机号码: 13166666666 所在地区: 上海市黄浦区 详细地址: 南苏州路333号中国东方航空公司\n";
 		UserInfo userInfoA1 = SmartAgent.parseUserAddressString(textA1).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoA1));
-
+		Console.log("Timer A1 took {} ms", timer.intervalMs("A1"));
+		
 		timer.start("A2");
 		String textA2 = "收货人: 李丽 手机号码: 13166666666 地址: 上海市黄浦区 详细地址: 南苏州路333号中国东方航空公司";
 		UserInfo userInfoA2 = SmartAgent.parseUserAddressString(textA2).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoA2));
-
+		Console.log("Timer A2 took {} ms", timer.intervalMs("A2"));
+		
 		timer.start("A3");
 		String textA3 = "姓名: 李丽 电话: 13166666666 地址: 上海市黄浦区 详细地址: 南苏州路333号中国东方航空公司";
 		UserInfo userInfoA3 = SmartAgent.parseUserAddressString(textA3).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoA3));
-
+		Console.log("Timer A3 took {} ms", timer.intervalMs("A3"));
+		
 		timer.start("B1");
 		String textB1 = "李丽13166666666上海市黄浦区南苏州路333号中国东方航空公司";
 		UserInfo userInfoB1 = SmartAgent.parseUserAddressString(textB1).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoB1));
-
+		Console.log("Timer B1 took {} ms", timer.intervalMs("B1"));
+		
 		timer.start("B2");
 		String textB2 = "上海市黄浦区南苏州路333号中国东方航空公司13166666666李丽";
 		UserInfo userInfoB2 = SmartAgent.parseUserAddressString(textB2).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoB2));
-
+		Console.log("Timer B2 took {} ms", timer.intervalMs("B2"));
+		
 		timer.start("C1");
 		String textC1 = "上海市黄浦区南苏州路333号中国东方航空公司";
 		UserInfo userInfoC1 = SmartAgent.parseUserAddressString(textC1).getUserInfo();
 		System.out.println(JsonUtil.toJsonStr(userInfoC1));
-		
-		
-		
-		Console.log("Timer A1 took {} ms", timer.intervalMs("A1"));
-		Console.log("Timer A2 took {} ms", timer.intervalMs("A2"));
-		Console.log("Timer A3 took {} ms", timer.intervalMs("A3"));
-		Console.log("Timer B1 took {} ms", timer.intervalMs("B1"));
-		Console.log("Timer B2 took {} ms", timer.intervalMs("B2"));
 		Console.log("Timer C1 took {} ms", timer.intervalMs("C1"));
 		
-		
-		
-		
+
 
 	}
 
